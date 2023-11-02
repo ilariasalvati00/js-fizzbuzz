7c14e5dda298966c403877dea09a14db60c7e952
Ilaria Salvati <ilariasalvati00@gmail.com>
2023-11-02 01:33:23 +0100

    fatto bonus 2

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
+const container = document.getElementById("container")
 for (let i = 1; i<=100; i++){
     if ((i%3==0) && (i%5==0)){
-        console.log("FizzBuzz");
+        container.innerHTML+='<div class="box fizzbuzz">fizzbuzz</div>'
     }
     else if (i%5==0){
-        console.log("Buzz");
+        container.innerHTML+='<div class="box buzz">buzz</div>'
     }
     else if (i%3==0) {
-        console.log("Fizz");
+        container.innerHTML+='<div class="box fizz">fizz</div>'
     }
     else{
-        console.log(i)
+        container.innerHTML+=`<div class="box numero">${i}</div>`
     }
 }
